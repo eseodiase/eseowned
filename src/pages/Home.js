aada6151deb0c0c94c02d7d263203e1c5e0be85e
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-27 12:49:27 +0100

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,24 @@
-import React from "react";
-const Home = () => {
-    return <h1>Home</h1>;
-    };
+import React from 'react';
+import {useState} from 'react';
+import axios from 'axios';
 
-    export default Home;
+function Home(){
+    const [quote, setQuote] = useState('')
+    const getQuote = () =>{
+        axios.get('https://api.quotable.io/radom').then (res =>{
+            console.log(res.data.content)
+            setQuote(res.data.content)
+        }).catch (err =>{
+            console.log(err)
+        })
+    }
+    
+    return(
+    <div className="App">
+    <button onClick={getQuote}> Get Quote </button>
+    (quote && <p>{quote}</p> )
+    </div>
+    )
+}
+
+export default Home;
